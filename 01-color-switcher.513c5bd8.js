65977a7f8e8e907a71bd871e9a55192d1cd4f251
Wargoth-ks <121107554+Wargoth-ks@users.noreply.github.com>
2024-05-30 11:50:28 +0000

    Deploying to gh-pages from @ Wargoth-ks/goit-js-hw-09@59a3d5befd38dfcf14883818739afaae5b44c269 🚀

---
+++ 01-color-switcher.513c5bd8.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=document.body.style;t.addEventListener("click",(()=>{t.disabled=!0,timerId=setInterval((()=>{d.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,console.log("Button disabled",t.disabled)}),1e3)})),e.addEventListener("click",(()=>{clearInterval(timerId),t.disabled=!1,console.log("Button disabled",t.disabled)}));
+//# sourceMappingURL=01-color-switcher.513c5bd8.js.map
