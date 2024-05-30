59a3d5befd38dfcf14883818739afaae5b44c269
Wargoth-ks <warheart1986@gmail.com>
2024-05-30 14:50:26 +0300

    Done 1st part HW

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+let bodyStyle = document.body.style;
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    timerId = setInterval(() => {
+        bodyStyle.background = getRandomHexColor();
+        console.log('Button disabled', startButton.disabled);
+    }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+    clearInterval(timerId);
+    startButton.disabled = false;
+    console.log('Button disabled', startButton.disabled);
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
