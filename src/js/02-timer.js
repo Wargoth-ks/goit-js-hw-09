9bffe006d435268d60664955c54d7305e9094f7e
Wargoth-ks <warheart1986@gmail.com>
2024-05-31 07:40:03 +0300

    Add notify

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const dateInp = document.querySelector('#datetime-picker');
 const btn = document.querySelector('button[data-start]');
@@ -9,6 +10,10 @@ const timeData = {
     hours: document.querySelector('.value[data-hours]'),
     minutes: document.querySelector('.value[data-minutes]'),
     seconds: document.querySelector('.value[data-seconds]'),
+};
+
+const notifyFontSize = {
+    fontSize: '16px',
 };
 
 let timerId = null;
@@ -21,27 +26,31 @@ const options = {
     onClose() {
         if (this.selectedDates[0] <= this.config.defaultDate) {
             btn.disabled = true;
+            Notify.failure('Please choose a date in the future', notifyFontSize);
             return;
         }
         btn.disabled = false;
-        btn.addEventListener('click', () => {
-            timerId = setInterval(() => {
-                let delta = this.selectedDates[0] - new Date();
-                if (delta > 0) {
-                    const { days, hours, minutes, seconds } = convertMs(delta);
-                    timeData.days.textContent = addLeadingZero(days);
-                    timeData.hours.textContent = addLeadingZero(hours);
-                    timeData.minutes.textContent = addLeadingZero(minutes);
-                    timeData.seconds.textContent = addLeadingZero(seconds);
-                } else {
-                    clearInterval(timerId);
-                }
-            }, 1000);
-        });
+        btn.addEventListener('click', onClick);
     },
 };
 
-flatpickr(dateInp, options);
+const fl = flatpickr(dateInp, options);
+
+const onClick = _ => {
+    timerId = setInterval(() => {
+        let delta = fl.selectedDates[0] - new Date();
+        if (delta > 0) {
+            const { days, hours, minutes, seconds } = convertMs(delta);
+            timeData.days.textContent = addLeadingZero(days);
+            timeData.hours.textContent = addLeadingZero(hours);
+            timeData.minutes.textContent = addLeadingZero(minutes);
+            timeData.seconds.textContent = addLeadingZero(seconds);
+        } else {
+            clearInterval(timerId);
+            Notify.success('Done!!!', notifyFontSize);
+        }
+    }, 1000);
+};
 
 function addLeadingZero(value) {
     return value.toString().padStart(2, '0');
