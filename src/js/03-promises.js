142511b5e5bc681bd1c025eff03af5fe58b087b8
Wargoth-ks <warheart1986@gmail.com>
2024-05-31 11:34:51 +0300

    Add 3rd part HW

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const btn = document.querySelector('.form');
+console.dir(btn);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    return new Promise((resolve, reject) => {
+        const shouldResolve = Math.random() > 0.3;
+        setTimeout(() => {
+            if (shouldResolve) {
+                resolve({ position, delay });
+            } else {
+                reject({ position, delay });
+            }
+        }, delay);
+    });
 }
+
+function onClick(event) {
+    event.preventDefault();
+    const { delay, step, amount } = event.currentTarget.elements;
+    let inputDelay = Number(delay.value);
+    let inputStep = Number(step.value);
+    let inputAmount = Number(amount.value);
+
+    for (let i = 1; i <= inputAmount; i += 1) {
+        inputDelay += inputStep;
+
+        createPromise(i, inputDelay)
+            .then(({ position, delay }) => {
+                Notify.success(
+                    `✅ Fulfilled promise ${position} in ${delay}ms`
+                );
+            })
+            .catch(({ position, delay }) => {
+                Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+            });
+        event.currentTarget.reset();
+    }
+}
+btn.addEventListener('click', onClick);
